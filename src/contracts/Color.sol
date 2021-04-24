b7232e4c190f6d8d226aea9bb527556062b46bd7
Flerio <nomnom2105@hotmail.com>
2021-04-24 17:41:16 +0200

    added ability to mint new tokens

--- src/contracts/Color.sol
+++ src/contracts/Color.sol
@@ -2,10 +2,27 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract Color is ERC721 {
+contract Color is ERC721Enumerable {
+    string[] public colors;
+    mapping(string => bool) _colorExists;
 
     constructor() ERC721("Color", "COLOR") {
+    }
 
+    // we would like to restrict the minter to one or more persons
+    function mint(string memory _color) public {
+        
+
+        // check if unique
+        require(!_colorExists[_color]);
+
+        colors.push(_color);
+        uint _id = colors.length;
+        _mint(msg.sender, _id);
+
+        // can i use the colors array to check if the color already exists and should not be minted?
+        _colorExists[_color] = true;
     }
 }
--- test/Color.test.js
+++ test/Color.test.js
@@ -9,11 +9,71 @@ require("chai")
 contract("Color", (accounts) => {
     let contract;
 
+    before(async() => {
+        contract = await Color.deployed();
+    });
+
     describe("deployment", async() => {
         it("is deployed", async () => {
-            contract = await Color.deployed();
             const address = contract.address;
             assert.notEqual(address, "");
+            assert.notEqual(address, 0x0);
+            assert.notEqual(address, null);
+            assert.notEqual(address, undefined);
+        });
+
+        it("has a name", async() => {
+            const name = await contract.name();
+            assert.equal(name, "Color");
+        });
+
+        it("has a symbol", async() => {
+            const symbol = await contract.symbol();
+            assert.equal(symbol, "COLOR");
+        });
+    });
+
+    describe("minting", async() => {
+        it("creates a new token", async() => {
+            const result = await contract.mint("#EC058E");
+            const totalSupply = await contract.totalSupply();
+            const event = result.logs[0].args;
+
+            assert.equal(totalSupply, 1);
+            assert.equal(event.tokenId.toNumber(), 1, "id is wrong");
+            assert.equal(event.from, 0x0000000000000000000000000000000000000000, "from is wrong");
+            assert.equal(event.to, accounts[0], "to is wrong");
+        });
+
+        it("only creates unique tokens", async () => {
+            await contract.mint("#FFFFFF").should.be.fulfilled;
+            await contract.mint("#FFFFFF").should.be.rejected;
+        });
+    });
+
+    describe("indexing", async() => {
+        it("lists all colors", async() => {
+            const promise1 = contract.mint("#5386E4");
+            const promise2 = contract.mint("#FFFFF1");
+            const promise3 = contract.mint("#000000");
+
+            await Promise.all([promise1, promise2, promise3]).should.be.fulfilled;
+            const totalSupply = await contract.totalSupply();
+
+            let color;
+            let result = [];
+
+            for(let i = 1; i <= totalSupply; i++) {
+                color = await contract.colors(i - 1);
+                result.push(color);
+            }
+
+            const expected = ["#5386E4", "#EC058E","#FFFFFF", "#FFFFF1", "#000000"]
+
+            result.sort();
+            expected.sort();
+            
+            assert.equal(result.join(","), expected.join(","));
         });
     });
 });
