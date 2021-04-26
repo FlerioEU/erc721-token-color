ff1c0084b944a909924bd678a222afda9108184a
Flerio <26186240+FlerioEU@users.noreply.github>
2021-04-26 22:27:36 +0200

    cleaning up some leftovers from template

--- src/components/App.js
+++ src/components/App.js
@@ -77,12 +77,7 @@ class App extends Component {
     return (
       <div>
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className="navbar-brand col-sm-3 col-md-2 mr-0">
             Color Tokens
           </a>
           <ul className="navbar-nav px-3">
